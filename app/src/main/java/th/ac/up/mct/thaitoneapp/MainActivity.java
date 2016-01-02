a291ee8f8c99471564ac74f06a1a2a73a39a0943
Chaow Porkaew <pchaowmobile@gmail.com>
2016-01-02 12:14:03 +0700

    add actriveandroid

--- app/src/main/java/th/ac/up/mct/thaitoneapp/MainActivity.java
+++ app/src/main/java/th/ac/up/mct/thaitoneapp/MainActivity.java
@@ -5,12 +5,16 @@ import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.activeandroid.ActiveAndroid;
+
 public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActiveAndroid.initialize(this);
     }
 
     @Override
